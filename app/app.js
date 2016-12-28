b219fed09a344c89bab2a80454dc58cd17e00494
Daniel Glejzner <monobloss@gmail.com>
2016-12-28 23:07:39 +0100

    prototype

---
+++ app/app.js
@@ -0,0 +1,51 @@
+var myMarvelHeroesApp = angular.module('myMarvelHeroesApp', ['ngRoute', 'ngAnimate']);
+
+myMarvelHeroesApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $routeProvider
+
+.when('/home', {templateUrl: "views/home.html"})
+
+.when('/heroesapp', {templateUrl: "views/heroesapp.html", controller: "HeroesController"
+
+                    }).otherwise({
+        redirectTo: '/home'
+    });
+
+}]);
+
+
+myMarvelHeroesApp.controller('HeroesController', ['$scope', '$http', function($scope, $http){
+
+   	$scope.isActive = false;
+  $scope.activeButton = function() {
+    $scope.isActive = !$scope.isActive;
+  }  
+    
+    
+    $scope.removeHero = function(hero){
+        var removedHero = $scope.heroes.indexOf(hero);
+        $scope.heroes.splice(removedHero, 1);
+    }
+ 
+ $scope.addHero = function(){
+     $scope.heroes.push({
+         name: $scope.newhero.name,
+         power: $scope.newhero.power,
+         origin: $scope.newhero.origin,
+        wealth: parseInt($scope.newhero.wealth),
+        selected: true
+        });
+            $scope.newhero.name = "";
+         $scope.newhero.power = "";
+         $scope.newhero.origin = "";
+      $scope.newhero.wealth = "";
+ }
+  $http.get('app/lib/data.json').then(function(response){
+  
+  $scope.heroes = response.data;
+});
+
+    
+    
+}]);
